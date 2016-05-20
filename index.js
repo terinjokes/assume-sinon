c551cb57d277ed0ec7b6c5d3252f57cecfd59b09
Arnout Kazemier <info@3rd-Eden.com>
2016-05-20 22:55:31 +0200

    [minor] Added missing JSDoc comments

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 'use strict';
 
-module.exports = function(assume, util) {
-  assume.flags.consistently = 'always';
-
+/**
+ * Expose the Assume plugin interface
+ *
+ * @param {Assume} assume The Assume instance.
+ * @param {Object} util Utilities provided by assume
+ * @public
+ */
+module.exports = function plugin(assume, util) {
   var format = util.format;
 
+  //
+  // Introduce a new flag.
+  //
+  assume.flags.consistently = 'always';
+
+  /**
+   * Assert if the given function is a sinon spy.
+   *
+   * @param {Function} maybeSpy Thing to inspect
+   * @returns {Boolean}
+   * @private
+   */
   function isSpy(maybeSpy) {
     return util.type(maybeSpy) === 'function'
       && util.type(maybeSpy.getCall) === 'function'
       && util.type(maybeSpy.calledWithExactly) === 'function';
   }
 
+  /**
+   * Assert if the given function might be a spy.
+   *
+   * @param {Function} maybeSpylike Thing that probably is a spy.
+   * @returns {Boolean}
+   * @private
+   */
   function isSpylike(maybeSpylike) {
     return maybeSpylike && (isSpy((maybeSpylike)) || isSpy(maybeSpylike.proxy));
   }
 
+  /**
+   * Extract the name of the spy.
+   *
+   * @param {Function} maybeSpy Possible sinon spy.
+   * @returns {String} Name of the function
+   */
   function maybeSpyName(maybeSpy) {
-    if (util.type(maybeSpy) === 'function') {
-      return util.name(maybeSpy);
-    } else {
-      return util.string(maybeSpy);
-    }
+    return util.type(maybeSpy) === 'function'
+    ? util.name(maybeSpy)
+    : util.string(maybeSpy);
   }
 
+  /**
+   * Generates human readable times from a numeric value.
+   *
+   * @param {Number} count Value to make readable
+   * @returns {String}
+   * @private
+   */
   function timesInWords(count) {
     if (count === 1) {
       return 'once';
@@ -39,16 +74,31 @@ module.exports = function(assume, util) {
     return (count || 0) + 'times';
   }
 
-  assume.add('spylike', function(msg) {
+  /**
+   * Assert if the value is spy.
+   *
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('spylike', function spylike(msg) {
     var value = this.value;
     var name = maybeSpyName(value);
 
     var expect = format('`%s` to @ be a Sinon.JS spy', name);
 
-    this.test(isSpylike(value), msg, expect);
+    return this.test(isSpylike(value), msg, expect);
   });
 
-  assume.add('called', function(count, msg) {
+  /**
+   * Assert that the function called x amount of times
+   *
+   * @param {Number} count Amount of calls expected.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('called', function called(count, msg) {
     if (util.type(count) === 'string') {
       msg = count;
       count = null;
@@ -68,7 +118,14 @@ module.exports = function(assume, util) {
     return this.test(this.value.called, msg, format(expect + 'at least once') );
   });
 
-  assume.add('calledWithNew', function(msg) {
+  /**
+   * Assert that the function called with `new`
+   *
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledWithNew', function calledWithNew(msg) {
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
@@ -80,10 +137,18 @@ module.exports = function(assume, util) {
       methodName = 'alwaysCalledWithNew';
     }
 
-    this.test(this.value[methodName](), msg, format(expect));
+    return this.test(this.value[methodName](), msg, format(expect));
   });
 
-  assume.add('calledBefore', function(spy2, msg) {
+  /**
+   * Assert that the function called before a given sinon spy.
+   *
+   * @param {Spy} spy2 Function to be called before.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledBefore', function calledBefore(spy2, msg) {
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
@@ -95,10 +160,18 @@ module.exports = function(assume, util) {
       methodName = 'alwaysCalledBefore';
     }
 
-    this.test(this.value[methodName](spy2), msg, format(expect, maybeSpyName(spy2)));
+    return this.test(this.value[methodName](spy2), msg, format(expect, maybeSpyName(spy2)));
   });
 
-  assume.add('calledAfter', function(spy2, msg) {
+  /**
+   * Assert that the function called after a given sinon spy.
+   *
+   * @param {Spy} spy2 Function to be called before.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledAfter', function calledAfter(spy2, msg) {
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
@@ -110,10 +183,18 @@ module.exports = function(assume, util) {
       methodName = 'alwaysCalledAfter';
     }
 
-    this.test(this.value[methodName](spy2), msg, format(expect, maybeSpyName(spy2)));
+    return this.test(this.value[methodName](spy2), msg, format(expect, maybeSpyName(spy2)));
   });
 
-  assume.add('calledOn', function(obj, msg) {
+  /**
+   * Assert that the function called on a given object.
+   *
+   * @param {Object} obj Object to be called with.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledOn', function calledOn(obj, msg) {
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
@@ -125,11 +206,18 @@ module.exports = function(assume, util) {
       methodName = 'alwaysCalledOn';
     }
 
-    this.test(this.value[methodName](obj), msg, format(expect, obj));
+    return this.test(this.value[methodName](obj), msg, format(expect, obj));
   });
 
-  assume.add('calledWith', function() {
+  /**
+   * Assert that the function called with the given arguments.
+   *
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledWith', function calledWith() {
     var args = Array.prototype.slice.call(arguments);
+
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
@@ -142,61 +230,88 @@ module.exports = function(assume, util) {
       methodName = 'alwaysCalledWith';
     }
 
-    this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
+    return this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
   });
 
-  assume.add('calledWithMatch', function() {
+  /**
+   * Assert that the function called with matching arguments.
+   *
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledWithMatch', function calledWithMatch() {
     var args = Array.prototype.slice.call(arguments);
+
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
 
     var methodName = 'calledWithMatch';
-
     var expect = 'spy to @ been called with arguments matching %j, called with %j';
 
     if (this.consistently) {
       methodName = 'alwaysCalledWithMatch';
     }
 
-    this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
+    return this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
   });
 
-  assume.add('calledWithExactly', function() {
+  /**
+   * Assert that the function called with exactly matching arguments.
+   *
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('calledWithExactly', function calledWithExactly() {
     var args = Array.prototype.slice.call(arguments);
+
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
 
     var methodName = 'calledWithExactly';
-
     var expect = 'spy to @ been called with exact arguments %j, called with %j';
 
     if (this.consistently) {
       methodName = 'alwaysCalledWithExactly';
     }
 
-    this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
+    return this.test(this.value[methodName].apply(this.value, args), null, format(expect, args, this.value.args));
   });
 
-  assume.add('returned', function(returnee, msg) {
+  /**
+   * Assert that the function has returned a given value.
+   *
+   * @param {Mixed} returnee Value that the function should return.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('returned', function returned(returnee, msg) {
     assume(this.value, {
       slice: this.sliceStack + 1
     }).to.be.spylike();
 
     var methodName = 'returned';
-
     var expect = 'spy to have @ retured %j, returns %j';
 
     if (this.consistently) {
       methodName = 'alwaysReturned';
     }
 
-    this.test(this.value[methodName](returnee), msg, format(expect, returnee, this.value.returnValues));
+    return this.test(this.value[methodName](returnee), msg, format(expect, returnee, this.value.returnValues));
   });
 
 
-  assume.add('thrown', function(err, msg) {
+  /**
+   * Assert that the function has thrown.
+   *
+   * @param {Object} err Object/string to throw.
+   * @param {String} msg Reason of assertion failure.
+   * @returns {Assume}
+   * @public
+   */
+  assume.add('thrown', function thrown(err, msg) {
     if (!msg && util.type(err) === 'string') {
       msg = err;
       err = null;
@@ -211,7 +326,6 @@ module.exports = function(assume, util) {
     }).to.be.spylike();
 
     var methodName = 'threw';
-
     var expect = 'spy to have @ threw ';
 
     if (this.consistently) {
@@ -223,6 +337,6 @@ module.exports = function(assume, util) {
       return this.test(this.value[methodName](err), msg, expect);
     }
 
-    this.test(this.value[methodName](), msg, format(expect, err, this.value.exceptions));
+    return this.test(this.value[methodName](), msg, format(expect, err, this.value.exceptions));
   });
 };
